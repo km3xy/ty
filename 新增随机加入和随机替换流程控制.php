354e1215531b8511a5a40c8660bdee2c1e2fc15f
km3xy <77920834+km3xy@users.noreply.github.com>
2023-04-28 09:21:09 +0800

    Update 新增随机加入和随机替换流程控制.php

--- 新增随机加入和随机替换流程控制.php
+++ 新增随机加入和随机替换流程控制.php
@@ -87,6 +87,103 @@ $("#bb").attr('id','aa');
 //$("#to").attr('id','xx');//之后变成xx
  
 
+  });
+});
+
+
+//前区
+
+</script>
+
+
+<script type="text/javascript">
+
+
+
+
+$(document).ready(function(){
+  $("#button1").click(function(){
+    
+
+
+var colors = ['a','b','c','d','e','f','g'];
+
+
+
+
+var x = colors[(Math.floor(Math.random() * (colors.length)))]
+
+
+
+
+var colorsxy = [,'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','1','2','3','4','5','6','7','8','9'];
+
+
+
+var xy = colorsxy[(Math.floor(Math.random() * (colorsxy.length)))]
+
+var y2="+";
+
+//$("#to").attr('id','xx');//之后变成xx
+ 
+ 
+ 
+ var $p= Math.floor(Math.random()*35+0);
+
+var $z= Math.floor(Math.random()*35+0);
+
+
+if ($p<$z)
+{
+    
+$("#"+x).val(Math.floor(Math.random()*35+1)+y2);
+
+}
+else
+{
+
+
+
+$("#"+x).after(Math.floor(Math.random()*35+1)+y2);
+
+
+  }
+
+
+ 
+ 
+
+ //前区
+
+
+
+
+$("#a").attr('id','g');
+
+$("#b").attr('id','f');
+
+
+$("#c").attr('id','e');
+
+
+$("#d").attr('id','c');
+
+$("#e").attr('id','d');
+
+
+$("#f").attr('id','b');
+
+
+$("#g").attr('id','a');
+
+
+
+  });
+});
+</script>
+
+
+
 
 
 
