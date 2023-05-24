c2b5c1a80be638f25ef7512f675b89e642435f10
km3xy <77920834+km3xy@users.noreply.github.com>
2023-05-24 23:29:17 +0800

    Create 新增核心input免输入版ms5新增，请选择上期码  .php

---
+++ 新增核心input免输入版ms5新增，请选择上期码  .php
@@ -0,0 +1,580 @@
+
+<a  style="font-size:55px;color:#FF0000"  style=" text-decoration: none;"     href="#" rel="external nofollow" rel="external nofollow" rel="external nofollow" id="AddMoreFileBox" class="btn btn-info">添加更多的input输入框</a></span></p>
+
+
+
+ <h1 style="font-size:60px;color:#FF0000"  id="">#option value</h1>
+
+
+
+
+ <h1 style="font-size:60px;color:#FF0000"  id="">option value 免输入版请选择上期码</h1>
+ 
+ 
+ 
+<p><strong>注意:</strong> Internet Explorer 9（更早 IE 版本），Safari 不支持 datalist 标签。</p>
+ 
+ 
+
+<div id="InputsWrapper">
+
+
+<div><b><input   list="browsers"   style="font-size:30px;color:#FF0000"  type="text" name="a[]" id="a"  value=""/>
+
+<datalist id="browsers">
+ 
+ <option value="1">
+  
+<option value="2">
+  
+<option value="3">
+  
+<option value="4">
+ 
+ <option value="5">
+  
+<option value="6">
+
+  <option value="7">
+
+  <option value="8">
+
+  <option value="9">
+
+  <option value="10">
+
+  <option value="11">
+
+  <option value="12">
+ 
+  <option value="13">
+
+  <option value="14">
+
+  <option value="15">
+
+  <option value="16">
+
+  <option value="17">
+
+  <option value="18">
+
+  <option value="19">
+
+  <option value="20">
+
+  <option value="21">
+
+  <option value="22">
+
+  <option value="23">
+
+  <option value="24">
+
+  <option value="25">
+
+  <option value="26">
+
+  <option value="27">
+
+  <option value="28">
+
+  <option value="29">
+
+  <option value="30">
+
+  <option value="31">
+
+  <option value="32">
+
+  <option value="33">
+
+  <option value="34">
+
+  <option value="35">
+
+</datalist>
+
+<a href="#" rel="external nofollow" rel="external nofollow" rel="external nofollow" class="removeclass"></b><input  style="font-size:30px;color:#FF0000"  type='button' value='删除'></a></div>
+</div>
+
+
+<div id="InputsWrapper">
+
+<div><b><input    list="browsers"  style="font-size:30px;color:#FF0000"  type="text" name="a[]" id="c" value=""/>
+
+
+<datalist id="browsers">
+ 
+ <option value="1">
+  
+<option value="2">
+  
+<option value="3">
+  
+<option value="4">
+ 
+ <option value="5">
+  
+<option value="6">
+
+  <option value="7">
+
+  <option value="8">
+
+  <option value="9">
+
+  <option value="10">
+
+  <option value="11">
+
+  <option value="12">
+ 
+  <option value="13">
+
+  <option value="14">
+
+  <option value="15">
+
+  <option value="16">
+
+  <option value="17">
+
+  <option value="18">
+
+  <option value="19">
+
+  <option value="20">
+
+  <option value="21">
+
+  <option value="22">
+
+  <option value="23">
+
+  <option value="24">
+
+  <option value="25">
+
+  <option value="26">
+
+  <option value="27">
+
+  <option value="28">
+
+  <option value="29">
+
+  <option value="30">
+
+  <option value="31">
+
+  <option value="32">
+
+  <option value="33">
+
+  <option value="34">
+
+  <option value="35">
+
+</datalist>
+
+
+
+<a href="#" rel="external nofollow" rel="external nofollow" rel="external nofollow" class="removeclass"></b><input  style="font-size:30px;color:#FF0000"  type='button' value='删除'></a></div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
